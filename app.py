48c6fa3a9ca378129c59cdfdaf7e3ebc253029bf
Tiago Jasper Zuege <tiago.zuege@gmail.com>
2017-11-14 15:42:34 -0200

    Inclusão da view de login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 from dataset import getData, getArtigos
 
 data = getData()
@@ -6,15 +6,43 @@ articles = getArtigos()
 
 app = Flask(__name__)
 
+
+def checkLogin(param, param2):
+    ok = False
+    if param == 'admin' and param2 == 'senha':
+        print('Login aceito!')
+        ok = True
+    else:
+        print('Login recusado! Cheque os seus dados.')
+        ok = False
+    return ok
+
+
+
+# Teste para verificar parametros de request
+
 def check_paramaters(parameters):
     if parameters == 'ok':
         print('yes, the parameter its correct!')
     else:
         print('oh no, invalid parameter!')
 
+
+# Rotas
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        ok = checkLogin(request.form['usr'], request.form['pwd'])
+        if ok:
+            return redirect('/')
+    return render_template('login.html')
+
 
 @app.route('/artigos', methods=['POST', 'GET'])
 def artigos():
@@ -22,10 +50,14 @@ def artigos():
         print('Here was a post')
     return render_template('artigos.html', artigos = articles)
 
+
+
 @app.route('/artigo/<string:id>')
 def artigo(id):
     return render_template('artigo.html', id=id)
 
+
+#Teste para verificar parametros enviados por POST
 @app.route('/teste', methods=['POST', 'GET'])
 def teste():
     if request.method == 'POST':
