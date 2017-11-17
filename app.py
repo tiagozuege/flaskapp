a7e71c01e2360c02ac34f9dfcf79baa922701f54
Tiago Jasper Zuege <tiago.zuege@gmail.com>
2017-11-17 17:10:49 -0200

    Adicionado suporte ao MySQL

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, redirect, render_template, request
+from flask_mysqldb import MySQL
 from dataset import getData, getArtigos
 
 data = getData()
 articles = getArtigos()
 
 app = Flask(__name__)
+
+# Config MySQL
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mysql'
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Init MySQL
+
+mysql = MySQL(app)
 
 
 def checkLogin(param, param2):
@@ -59,7 +72,7 @@ def artigo(id):
     return render_template('artigo.html', id=id)
 
 
-#Teste para verificar parametros enviados por POST
+# Teste para verificar parametros enviados por POST
 @app.route('/teste', methods=['POST', 'GET'])
 def teste():
     if request.method == 'POST':
@@ -67,3 +80,26 @@ def teste():
         print(request.form['teste'])                    #Imprime os dados que vieram pelo POST
         check_paramaters(request.form['teste'])
     return render_template('teste.html', data = data)
+
+# Teste Mysql
+
+@app.route('/teste-mysql')
+def teste2():
+    l = []
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM usuarios''')
+    data = cur.fetchall()
+    #return data
+    return render_template('teste-mysql.html', data=data)
+
+
+# Exemplo de insert
+'''
+cur = mysql.connection.cursor()
+cur.execute('INSERT INTO... VALUES (%s, %s, %s)', (data1, d2, d3))
+
+mysql.connection.commit()
+
+cur.close()
+'''
+
